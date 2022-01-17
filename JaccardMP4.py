5743612fffc914fb783ad3263e58659c8d760b27
manzoor-ul-haq <manzoor.haq00@gmail.com>
2022-01-17 14:29:04 +0500

    Jaccard.mp4

---
+++ JaccardMP4.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+# choose codec according to format needed
+fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
+video = cv2.VideoWriter('video.avi', fourcc, 1, (640, 480))
+path = r'C:\Users\admin\Documents\Namal\Fall 2021\CSE-491 Final Year Project-1\Studying-Diversity-In-Genetic-Programming\Graphs\Generation_'
+
+for i in range(0,63):
+   img = cv2.imread(path + str(i) + '.png')
+   video.write(img)
+
+cv2.destroyAllWindows()
+video.release()
